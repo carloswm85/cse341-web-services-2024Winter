968322a0e8cc18f10d099caadc1a525ccc3ca18b
Carlos W. Mercado <carlos.washington.mercado@gmail.com>
2024-01-22 21:36:51 -0300

    update w02 personal assignment

--- w02/w02-student/w02-personal-assignment/controllers/contactsController.js
+++ w02/w02-student/w02-personal-assignment/controllers/contactsController.js
@@ -1,23 +1,43 @@
-const mongodbInstance = require('../db/connect');
+const mongodbInstance = require("../db/connect");
 
 const DATABASE = process.env.DATABASE_NAME;
 const COLLECTION = process.env.COLLECTION_NAME;
 
 const getData = async (req, res, next) => {
-	const result = await mongodbInstance.getDb()
-		.db(DATABASE).collection(COLLECTION).find();
-	
-		console.log("RESULT: ");
-		console.log('object :>> ', result);
-	
-	result.toArray().then((lists) => {
+	const result = await mongodbInstance
+		.getDb()
+		.db(DATABASE)
+		.collection(COLLECTION)
+		.find();
 
+	console.log("RESULT: ");
+	console.log("object :>> ", result);
+
+	result.toArray().then((lists) => {
 		console.log("LISTS: ");
 		console.log("object :>> ", lists);
-		
-		res.setHeader('Content-Type', 'application/json');
+
+		res.setHeader("Content-Type", "application/json");
 		res.status(200).json(lists);
 	});
 };
 
-module.exports = { getData };
+const getItem = async (req, res, next) => {
+	const result = await mongodbInstance
+		.getDb()
+		.db(DATABASE)
+		.collection(COLLECTION)
+		.find();
+
+	result.toArray().then((lists) => {
+		var itemId = lists[req.params.id];
+		if (itemId != undefined) {
+			res.setHeader("Content-Type", "application/json");
+			res.json(itemId);
+		} else {
+			res.status(404).json('Does not exist');
+		}
+	});
+};
+
+module.exports = { getData, getItem };
--- w02/w02-student/w02-personal-assignment/routes/contactsRoute.js
+++ w02/w02-student/w02-personal-assignment/routes/contactsRoute.js
@@ -6,6 +6,7 @@ const router = express.Router();
 
 // GET
 router.get('/lista', contactsController.getData);
+router.get('/:id', contactsController.getItem);
 
 // localhost:8080/contactos/
 module.exports = router;

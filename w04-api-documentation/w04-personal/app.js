ab9002b3be23c09fa5731d29b53f4e165c80e2dd
Carlos W. Mercado <carlos.washington.mercado@gmail.com>
2024-02-02 13:44:00 -0300

    add cors

--- w04-api-documentation/w04-personal/app.js
+++ w04-api-documentation/w04-personal/app.js
@@ -19,15 +19,15 @@ const app = express();
 
 app
 	.use(bodyParser.json())
-	// .use(cors())
+	.use(cors())
 	.use((req, res, next) => {
 		res.setHeader("Access-Control-Allow-Origin", "*");
-		res.setHeader(
-			"Access-Control-Allow-Headers",
-			// "Origin, X-Requested-With, Content-Type, Accept, Z-Key"
-			"Content-Type"
-		);
-		res.setHeader("Content-Type", "application/json");
+		// res.setHeader(
+		// 	"Access-Control-Allow-Headers",
+		// 	// "Origin, X-Requested-With, Content-Type, Accept, Z-Key"
+		// 	"Content-Type"
+		// );
+		// res.setHeader("Content-Type", "application/json");
 		res.setHeader(
 			"Access-Control-Allow-Methods",
 			"GET, POST, PUT, DELETE, OPTIONS"

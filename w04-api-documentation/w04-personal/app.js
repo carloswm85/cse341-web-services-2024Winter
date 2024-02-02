1f45fcf8003e26067c85f7b9cc9c3e541f9bea74
Carlos W. Mercado <carlos.washington.mercado@gmail.com>
2024-02-02 12:33:27 -0300

    update

--- w04-api-documentation/w04-personal/app.js
+++ w04-api-documentation/w04-personal/app.js
@@ -19,20 +19,20 @@ const app = express();
 
 app
 	.use(bodyParser.json())
-	// .use(cors())
-	.use((req, res, next) => {
-		res.setHeader("Access-Control-Allow-Origin", "*");
-		res.setHeader(
-			"Access-Control-Allow-Headers",
-			"Origin, X-Requested-With, Content-Type, Accept, Z-Key"
-		);
-		res.setHeader("Content-Type", "application/json");
-		res.setHeader(
-			"Access-Control-Allow-Methods",
-			"GET, POST, PUT, DELETE, OPTIONS"
-		);
-		next();
-	})
+	.use(cors())
+	// .use((req, res, next) => {
+	// 	res.setHeader("Access-Control-Allow-Origin", "*");
+	// 	res.setHeader(
+	// 		"Access-Control-Allow-Headers",
+	// 		"Origin, X-Requested-With, Content-Type, Accept, Z-Key"
+	// 	);
+	// 	res.setHeader("Content-Type", "application/json");
+	// 	res.setHeader(
+	// 		"Access-Control-Allow-Methods",
+	// 		"GET, POST, PUT, DELETE, OPTIONS"
+	// 	);
+	// 	next();
+	// })
 	.use("/docs-api", swaggerUi.serve, swaggerUi.setup(swaggerFile))
 	.use(`/`, routes);
 

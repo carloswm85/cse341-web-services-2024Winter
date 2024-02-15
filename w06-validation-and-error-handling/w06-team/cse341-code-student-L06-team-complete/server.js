06e950a1e0cfc80caf968fb770060a961fdfd74e
Carlos W. Mercado <carlos.washington.mercado@gmail.com>
2024-02-15 20:10:05 -0300

    UPDATE db name in .env variables

--- w06-validation-and-error-handling/w06-team/cse341-code-student-L06-team-complete/server.js
+++ w06-validation-and-error-handling/w06-team/cse341-code-student-L06-team-complete/server.js
@@ -12,7 +12,9 @@ app
   })
   .use('/', require('./routes'));
 
+// CATCH EXCEPTIONS
 process.on('uncaughtException', (err, origin) => {
+  // Add into a file if you want to
   console.log(process.stderr.fd, `Caught exception: ${err}\n` + `Exception origin: ${origin}`);
 });
 

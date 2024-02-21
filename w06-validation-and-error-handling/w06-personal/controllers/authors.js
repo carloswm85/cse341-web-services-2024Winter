0dda7bf3362d2fe40bcaa6cb095f0410881b5885
Carlos W. Mercado <carlos.washington.mercado@gmail.com>
2024-02-21 17:53:25 -0300

    ADD error handling in authors controller

--- w06-validation-and-error-handling/w06-personal/controllers/authors.js
+++ w06-validation-and-error-handling/w06-personal/controllers/authors.js
@@ -8,36 +8,49 @@ const COLLECTION = process.env.COLLECTION_ONE;
 const getData = async (req, res, next) => {
 	// #swagger.tags = ['Authors']
 
-	const response = await mongodbInstance
-		.getDb()
-		.db(DATABASE)
-		.collection(COLLECTION)
-		.find();
+	try {
+		const response = await mongodbInstance
+			.getDb()
+			.db(DATABASE)
+			.collection(COLLECTION)
+			.find();
 
-	response.toArray().then((lists) => {
-		res.setHeader("Content-Type", "application/json");
-		res.status(200).json(lists);
-	});
+		response.toArray().then((lists) => {
+			if (lists.length == 0) {
+				res.status(200).json({
+					message: "There is no content to display. Database is empty."
+				});
+			}
+			res.setHeader("Content-Type", "application/json");
+			res.status(200).json(lists);
+		});
+	} catch (err) {
+		res.status(500).json(err);
+	}
 };
 
 // GET ITEM
 const getItem = async (req, res, next) => {
 	// #swagger.tags = ['Authors']
 
-	var selectedId = req.params.id;
-	const collection = await mongodbInstance
-		.getDb()
-		.db(DATABASE)
-		.collection(COLLECTION);
+	try {
+		var selectedId = req.params.id;
+		const collection = await mongodbInstance
+			.getDb()
+			.db(DATABASE)
+			.collection(COLLECTION);
 
-	var response = await collection.findOne({ _id: new ObjectId(selectedId) });
+		var response = await collection.findOne({ _id: new ObjectId(selectedId) });
 
-	if (response != undefined) {
-		res.setHeader("Content-Type", "application/json");
-		res.json(response);
-		console.log("Item retrieved.");
-	} else {
-		res.status(404).json({ message: "Does not exist" });
+		if (response != undefined) {
+			res.setHeader("Content-Type", "application/json");
+			res.json(response);
+			console.log("Item retrieved.");
+		} else {
+			res.status(404).json({ message: "Does not exist" });
+		}
+	} catch (err) {
+		res.status(500).json(err);
 	}
 };
 
@@ -45,24 +58,42 @@ const getItem = async (req, res, next) => {
 const postItem = async (req, res, next) => {
 	// #swagger.tags = ['Authors']
 
-	const collection = await mongodbInstance
-		.getDb()
-		.db(DATABASE)
-		.collection(COLLECTION);
+	try {
+		if (
+			!req.body.author_name ||
+			!req.body.author_email ||
+			!req.body.author_image
+		) {
+			res.status(400).send({ message: "Content can not be empty!" });
+			return;
+		}
 
-	var newAuthor = {
-		author_name: req.body.author_name,
-		author_email: req.body.author_email,
-		author_image: req.body.author_image
-	};
+		const collection = await mongodbInstance
+			.getDb()
+			.db(DATABASE)
+			.collection(COLLECTION);
 
-	var response = await collection.insertOne(newAuthor);
+		var newAuthor = {
+			author_name: req.body.author_name,
+			author_email: req.body.author_email,
+			author_image: req.body.author_image,
+		};
 
-	if (response != undefined) {
-		res.status(201).json(response);
-		console.log(response.insertedId);
-	} else {
-		res.status(400).json({ message: "BAD REQUEST" });
+		var response = await collection.insertOne(newAuthor).catch((err) => {
+			res.status(500).send({
+				message:
+					err.message || "Some error occurred while creating the author.",
+			});
+		});
+
+		if (response != undefined) {
+			res.status(201).json(response);
+			console.log(response.insertedId);
+		} else {
+			res.status(400).json({ message: "BAD REQUEST" });
+		}
+	} catch (err) {
+		res.status(500).json(err);
 	}
 };
 
@@ -82,12 +113,12 @@ const putItem = async (req, res, next) => {
             }
   } */
 
-	const collection = await mongodbInstance
-		.getDb()
-		.db(DATABASE)
-		.collection(COLLECTION);
-
 	try {
+		const collection = await mongodbInstance
+			.getDb()
+			.db(DATABASE)
+			.collection(COLLECTION);
+
 		const selectedId = req.params._id;
 		// Create a filter for items with the selected id
 		const filter = { _id: new ObjectId(selectedId) };
@@ -101,6 +132,7 @@ const putItem = async (req, res, next) => {
 			$set: updatedAuthor,
 		};
 
+		// Iterate over all body keys
 		for (const key of Object.keys(req.body)) {
 			if (req.body[key] !== null) {
 				updatedAuthor[key] = req.body[key];
@@ -120,29 +152,24 @@ const putItem = async (req, res, next) => {
 		);
 
 		if (response != undefined) {
-			// Print the number of matching and modified documents
-			console.log(
-				`${response.matchedCount} document(s) matched the filter, updated ${response.modifiedCount} document(s)`
-			);
-			res.status(200);
-			res.json(response);
-			console.log("Item was updated.");
+			res.status(200).json(response);
 		}
-	} catch (error) {
-		res.status(400).json({ message: "BAD REQUEST" });
+	} catch (err) {
+		res.status(500).json(err);
 	}
 };
 
 // DELETE
 const deleteItem = async (req, res, next) => {
 	// #swagger.tags = ['Authors']
-	var selectedId = req.params._id;
-	const collection = await mongodbInstance
-		.getDb()
-		.db(DATABASE)
-		.collection(COLLECTION);
 
 	try {
+		var selectedId = req.params._id;
+		const collection = await mongodbInstance
+			.getDb()
+			.db(DATABASE)
+			.collection(COLLECTION);
+
 		// Delete the item by ID
 		const response = await collection.deleteOne({
 			_id: new ObjectId(selectedId),
@@ -151,13 +178,13 @@ const deleteItem = async (req, res, next) => {
 		if (response.deletedCount === 1) {
 			res.status(200).json({
 				message: `Item was deleted successfully.`,
+				response
 			});
 		} else {
 			res.status(404).json({ message: "Item not found" });
 		}
-	} catch (error) {
-		console.error(error);
-		res.status(500).json({ message: "Internal server error" });
+	} catch (err) {
+		res.status(500).json(err);
 	}
 };
 
